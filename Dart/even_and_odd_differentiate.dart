b6b5b4122768acd899a24f6e367d74e78d98bebf
sagarappscrip <sagarstark731999@gmail.com>
2023-10-24 12:31:16 +0530

    add : even & odd differentiate

---
+++ Dart/even_and_odd_differentiate.dart
@@ -0,0 +1,9 @@
+void main() {
+  int number = 5; // Change this number to check if it's even or odd
+
+  if (number % 2 == 0) {
+    print('$number is an even number.');
+  } else {
+    print('$number is an odd number.');
+  }
+}
